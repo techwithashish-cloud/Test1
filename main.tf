d0d42f0dd7393739b84dae65860e8c6a4734cf1f
techwithashish-cloud <aksingh.1586@gmail.com>
2025-03-09 17:46:35 +0530

    Update the codes at 17:46 pm for IAM CREATION

---
+++ main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+    region     = "${var.region}"    
+    access_key = "${var.access_key}"    
+    secret_key = "${var.secret_key}"    
+}
+resource "aws_iam_user" "user1" {
+  name = "John" 
+  path = "/"    
+  tags = {
+    tag-key = "Dev-Team"    
+  }
+}
+resource "aws_iam_user" "user2" {
+  name = "Sarah"    
+  path = "/"    
+  tags = {  
+    tag-key = "Dev-Team"        
+  }
+}
+resource "aws_iam_user" "user3" {   
+  name = "Rita" 
+  path = "/"    
+  tags = {
+    tag-key = "HR-Team"     
+  } 
+}
+resource "aws_iam_user" "user4" {
+  name = "Ted"      
+  path = "/"        
+  tags = {
+    tag-key = "HR-Team"   
+  }
+}
+resource "aws_iam_group" "group1" { 
+  name = "DevTeam"
+}
+resource "aws_iam_group_membership" "member1" {
+  name = "user1"    
+  users = [ 
+    aws_iam_user.user1.name,    
+    aws_iam_user.user2.name,        
+  ]     
+  group = aws_iam_group.group1.name     
+}
+resource "aws_iam_group" "group2" {
+  name = "HRTeam"       
+}
+resource "aws_iam_group_membership" "member2" { 
+  name = "user2"        
+  users = [     
+    aws_iam_user.user3.name,        
+    aws_iam_user.user4.name,        
+  ]     
+  group = aws_iam_group.group2.name     
+}
